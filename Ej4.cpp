7c21f52432922933e3d3d403b6c58d8853e00c00
AgustinaBosiaa <2324696@ucc.edu.ar>
2024-09-16 15:47:54 -0300

    Create Ej4.cpp

---
+++ Ej4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Arbol/ArbolBinario.h"
+using namespace std;
+/*  Problema: Un restaurante tiene un sistema de reservas donde cada mesa tiene un número
+específico. A medida que los clientes reservan o cancelan mesas, el sistema debe
+mantenerse actualizado.
+    ● Tarea: Agrega las reservas de las siguientes mesas: 15, 10, 20, 5, 12, 18, 25.
+    ● Desafío: Dos clientes cancelaron sus reservas en las mesas 10 y 20, así que
+elimínalas del sistema. Luego, un nuevo cliente reserva la mesa 13. Verifica si la
+mesa 12 aún está reservada e imprime la lista actualizada de mesas reservadas.
+*/
+
+int main(){
+    
+    return 0;
+}
